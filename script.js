d52f0d7a5581c476d0bbfcd59064952e9dde0188
sathesbitmaker <sathes352@gmail.com>
2021-02-08 15:12:22 -0500

    playingAround

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-var scroll_start = 0;
-//Scroll Function
-$(document).ready(function() {
-  $("#projects").on("click", function() {
-    var answer = $(".Title").offset().top
-      window.scroll({
-        top: answer - 140,
-        behavior: 'smooth'
-      })
-  })
-  $("#contact").on("click", function() {
-      window.scroll({
-        top: 9000,
-        behavior: 'smooth'
-      })
-  })
-})
+let dog = {
+    sound: 'woof',
+    talk: function() {
+        console.log(this.sound)
+    }
+}
+
+let talkFunction = dog.talk
+let boundFunction = 
+    talkFunction.bind(dog)
+boundFunction()
+
+button.addEventListener(
+    'click', 
+    dog.talk.bind(dog)
+)
