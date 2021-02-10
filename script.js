df5a3218c79c6b31128b55a99caf5227a354198b
sathesbitmaker <sathes352@gmail.com>
2021-02-10 11:37:41 -0500

    UpdateNetNinjaYoutubeVideoClone

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
+ 
 class User {
     constructor(email, name){
         this.email = email;
-        this.name= name;
+        this.name = name;
         this.score = 0;
     }
-    
     login(){
-        console.log(this.email, 'just logged in')
+        console.log(this.email, 'just logged in');
         return this;
     }
     logout(){
         console.log(this.email, 'just logged out');
+        return this;
     }
     updateScore(){
         this.score++;
         console.log(this.email, 'score is now', this.score);
+        return this;
     }
 }
 
 class Admin extends User {
-    deleteUser() {
-        
+    deleteUser(user){
+        users = users.filter(u => {
+            return u.email != user.email
+        });
     }
 }
 
-var userOne = new User('rye@ninjas.com', 'rye')
-var userTwo = new User ('yoshi@mariokorp.com', 'yoshi')
+var userOne = new User('ryu@ninjas.com', 'Ryu');
+var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
+var admin = new Admin('shaun@ninjas.com', 'Shaun');
+
+var users = [userOne, userTwo, admin];
+
+// admin.deleteUser(userTwo);
+userTwo.deleteUser(userOne); // won't work
+
+console.log(users);
